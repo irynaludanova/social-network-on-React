6dbd5bcfe1c02981344d9e8f676b3299bc55820f
LUdanova <irynaludanova@gmail.com>
2021-02-27 21:27:11 +0200

    Dispatch and action

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ function App(props) {
             render={() => (
               <Profile
                 profilePage={props.state.profilePage}
-                addPost={props.addPost}
-                updateNewPostText={props.updateNewPostText}
+                dispatch={props.dispatch}
               />
             )}
           />
--- src/redux/state.js
+++ src/redux/state.js
@@ -33,6 +33,21 @@ let store = {
   subscribe(observer) {
     this._callSubscriber = observer;
   },
+  dispatch(action) {
+    if (action.type === "ADD-POST") {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = "";
+      this._callSubscriber(this._state);
+    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
+  },
 };
 
 window.store = store;
