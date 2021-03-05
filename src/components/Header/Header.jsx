497537598ea494087d34cf9a7d21aedc6e7d2253
LUdanova <irynaludanova@gmail.com>
2021-03-05 15:08:32 +0200

    Login, auth/me, cookie

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={classes.header}>
       <div className={classes.rotateShadows}></div>
       <div className={classes.title}>
         <h1>village social network</h1>
+        <div className={classes.loginBlock}>
+          {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+        </div>
       </div>
     </header>
   );
